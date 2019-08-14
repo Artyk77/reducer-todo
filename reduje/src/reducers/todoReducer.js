b00b0666cb9db1fd978501c054f020694dc13b4d
jose restrepo <arturo.restrepo@gmail.com>
2019-08-13 21:22:26 -0500

    Add/Delete Added

--- reduje/src/reducers/todoReducer.js
+++ reduje/src/reducers/todoReducer.js
@@ -1,18 +1,42 @@
 export const initialState = {
-    todo: "Buy Groceries",
-    editing: false,
-}
 
+    todos: [
+    ],
+}
 
 export const todoReducer = (state, action) => {
     //Actions
-    switch(action.type) {
+    switch (action.type) {
         case 'NEW_TODO':
-        return{
-            ...state, 
-            editing: !state.editing
-        }
-        default: 
-        return state;
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {
+                        item: action.payload,
+                        completed: false,
+                        id: Date.now(),
+                    },
+                ],
+            };
+        case 'TOGGLE_TODO':
+            return {
+                ...state,
+                todos: state.todos.map(todo => {
+                    if (todo.id === action.payload.id) {
+                        return { ...todo, completed: !todo.completed };
+                    } else {
+                        return todo;
+                    }
+                }),
+            };
+        case 'REMOVE_TODO':
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.completed !== true),
+            };
+        default:
+            return state;
+
     }
-}
+};
